01521d80bc8ca01161fd9f4c483935a0c346f89d
leander.kuehnel@mail.de <leander.kuehnel@mail.de>
2026-02-12 13:40:34 +0100

    init

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from tidal-tui!")
+import requests, base64, json, mpv
 
+def search_song(q):
+    songs = requests.get(f"https://wolf.qqdl.site/search?s={q}").json()["data"]["items"]
+    return songs
 
-if __name__ == "__main__":
-    main()
+def play_song(id):
+    manifest = requests.get(f"https://wolf.qqdl.site/track?id={id}&quality=LOSSLESS").json()["data"]["manifest"]
+    url= json.loads(base64.b64decode(manifest.encode()).decode())["urls"][0]
+    return url
+
+song_id = search_song("the sound of silence simon and garfunkel")[0]
+print(song_id)
+
+song_id = song_id["id"]
+
+print(play_song(song_id))
